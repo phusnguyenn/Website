4a30eb54cdcfe13bca7f7770a14aeb31adf20349
PhuNQ <phu.nguyen.intern@ntq-solution.com.vn>
2018-05-08 16:47:40 +0700

    First commit

---
+++ BDS/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BDS.Models;
+
+namespace BDS.Controllers
+{
+    public class HomeController : Controller
+    {
+        HomeDataEntities2 data = new HomeDataEntities2();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            var listHouse = data.Houses.Take(9).ToList();
+            return View(listHouse);
+        }
+
+        public ActionResult ProductDetail(int houseId = 1)
+        {
+            House houseDetails = data.Houses.FirstOrDefault(house => house.HouseId == houseId);
+            if (houseDetails == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(houseDetails);
+
+        }
+    }
+}
